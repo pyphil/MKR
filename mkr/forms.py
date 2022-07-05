a8b685a192e863ef510720aab9338e16256e160b
pyphil <63059088+pyphil@users.noreply.github.com>
2022-07-05 23:14:32 +0200

    layout

--- mkr/forms.py
+++ mkr/forms.py
@@ -21,17 +21,17 @@ class KompetenzkarteForm(ModelForm):
             'download',
         )
         labels = {
-            'kategorie': mark_safe('<strong>Kategorie im Medienkompetenzrahmen:</strong>'),
-            'fach': mark_safe('<strong>Fach:</strong>'),
-            'jgst': mark_safe('<strong>Jahrgangsstufe:</strong>'),
-            'vorhaben': mark_safe('<strong>Unterrichtsvorhaben (Titel):</strong>'),
-            'info': mark_safe('<strong>Detaillierte Informationen befinden sich hier (z.B. schulinternes Curriculum):</strong>'),
-            'medienkompetenz': mark_safe('<strong>Medienkompetenz:</strong>'),
-            'technik': mark_safe('<strong>Technik:</strong>'),
+            'kategorie': mark_safe('<strong>Kategorie im Medienkompetenzrahmen</strong>'),
+            'fach': mark_safe('<strong>Fach</strong>'),
+            'jgst': mark_safe('<strong>Jahrgangsstufe</strong>'),
+            'vorhaben': mark_safe('<strong>Unterrichtsvorhaben (Titel)</strong>'),
+            'info': mark_safe('<strong>Detaillierte Informationen befinden sich hier (z.B. schulinternes Curriculum)</strong>'),
+            'medienkompetenz': mark_safe('<strong>Medienkompetenz</strong>'),
+            'technik': mark_safe('<strong>Technik</strong>'),
             'alle_teil': mark_safe('<strong>Das Vorhaben ist...</strong>'),
             'pflicht_empf': mark_safe('<strong>Das Vorhaben ist...</strong>'),
             'durchf_planung': mark_safe('<strong>Das Vorhaben ist...</strong>'),
-            'download': mark_safe('<strong>Dateianhang (wird nur im internen Bereich sichtbar)</strong>')
+            'download': mark_safe('<strong>Dateianhang für internen Bereich (Material, Konzept)</strong>')
         }
         widgets = {
             'kategorie': forms.Select(attrs={'class': 'form-select'}),
