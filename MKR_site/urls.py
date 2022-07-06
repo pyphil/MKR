474ef9ffe7590870b9cd5abd927d4724295a07eb
pyphil <63059088+pyphil@users.noreply.github.com>
2022-07-06 19:42:07 +0200

    login logout base.html

--- MKR_site/urls.py
+++ MKR_site/urls.py
@@ -21,4 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mkr.urls')),
+    path('register/', include('accounts.urls'), name='register'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
