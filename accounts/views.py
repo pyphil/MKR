49ca95706695cbac4d7894686f95f517b15ec125
pyphil <63059088+pyphil@users.noreply.github.com>
2022-07-07 01:09:57 +0200

    minimal registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        print(request.POST.get('username'))
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'registration/register.html', {'form': form})
