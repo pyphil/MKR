8e8c9c336a75d02a64e974a8993ff11130821a54
pyphil <63059088+pyphil@users.noreply.github.com>
2022-07-08 02:11:08 +0200

    adapted registration form with alert and email

--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterUserForm
 
 # Create your views here.
 def register(request):
+    email_error = False
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        print(request.POST.get('username'))
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.is_active = False
-            user.save()
+        form = RegisterUserForm(request.POST)
+        if request.POST.get('email') in ['abc@genm.info', 'test13@genm.info']:
+            if form.is_valid():
+                user = form.save(commit=False)
+                user.is_active = False
+                user.save()
+                # render info page about email confirmation
+        else:
+            email_error = True
     else:
-        form = UserCreationForm()
+        form = RegisterUserForm()
 
-    return render(request, 'registration/register.html', {'form': form})
+    return render(request, 'registration/register.html', {'form': form, 'email_error': email_error})
