7fd4075ecf8d1883e50396e41ec724386ffc13b0
pyphil <63059088+pyphil@users.noreply.github.com>
2022-12-01 08:05:08 +0100

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,8 @@ from .models import Profile, AllowedEmail
 from .forms import RegisterUserForm
 from uuid import uuid4
 from django.contrib.auth.models import User
+from threading import Thread
+from django.core.mail import send_mail
 
 
 def register(request):
@@ -20,6 +22,7 @@ def register(request):
                     user=user,
                     uuid=newuuid
                 )
+                # TODO: Send Mail
                 print("Confirmation uuid: ", newuuid)
                 # render info page about email confirmation
                 return redirect('registration_email')
@@ -46,3 +49,43 @@ def confirm_email(request, uuid):
         error = True
 
     return render(request, 'registration/email_success.html', {'error': error})
+
+
+class mail_thread(Thread):
+    # TODO: Anpassen
+    def __init__(self, student_ids):
+        super(mail_thread, self).__init__()
+        self.student_ids = student_ids
+        self.noreply = Config.objects.get(name="noreply-mail")
+
+    # run method is automatically executed on thread.start()
+    def run(self):
+        for id in self.student_ids:
+            student = Student.objects.get(id=id)
+            oldcode = student.code
+            # put oldcode on delete list if exists
+            if oldcode is not None:
+                CodeDeletion.objects.create(
+                    code_to_delete=oldcode,
+                    name=student.name,
+                    firstname=student.firstname,
+                    group=student.group
+                )
+            newcode = Code.objects.filter(type='y').first()
+            student.code = newcode.code
+            # delete used code
+            newcode.delete()
+            student.date = datetime.today()
+            student.save()
+            mail_text_obj = Config.objects.get(name='mail_text')
+            mail_text = mail_text_obj.text
+            mail_text = mail_text.replace('#NAME#', student.firstname)
+            mail_text = mail_text.replace('#CODE#', student.code)
+
+            send_mail(
+                'WLAN-CODE',
+                mail_text,
+                self.noreply,
+                [student.email],
+                fail_silently=True,
+            )
