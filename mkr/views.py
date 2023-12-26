d9609745d92aa14ae655d354755743783a93077d
pyphil <63059088+pyphil@users.noreply.github.com>
2023-12-26 18:13:08 +0100

    filter fach jgst / collapse and show

--- mkr/views.py
+++ mkr/views.py
@@ -1,22 +1,64 @@
 from django.shortcuts import render, redirect
 from .forms import KompetenzkarteForm
-from .models import Kompetenzkarte
+from .models import Kompetenzkarte, Fach
 from django.http import FileResponse
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 
 
 def home(request):
-    mkr_objects = Kompetenzkarte.objects.all()
-    if request.GET.get('cookiebutton'):
-        response = redirect('home')
-        response.set_cookie('cookiebanner', False)
-        return response
     try:
         cookiebanner = request.COOKIES['cookiebanner']
     except Exception:
         cookiebanner = True
-    return render(request, 'mkr.html', {'mkr_objects': mkr_objects, 'cookiebanner': cookiebanner})
+    if request.GET.get('cookiebutton'):
+        response = redirect('home')
+        response.set_cookie('cookiebanner', False)
+        return response
+
+    mkr_objects = Kompetenzkarte.objects.all()
+
+    if request.GET.get('fach_filter') and request.GET.get('fach_filter') != '0':
+        mkr_objects = mkr_objects.filter(fach__fach=request.GET.get('fach_filter'))
+        selected_fach = request.GET.get('fach_filter')
+    else:
+        selected_fach = None
+
+    if request.GET.get('jgst_filter') and request.GET.get('jgst_filter') != '0':
+        mkr_objects = mkr_objects.filter(jgst=request.GET.get('jgst_filter'))
+        selected_jgst = request.GET.get('jgst_filter')
+    else:
+        selected_jgst = None
+
+    # selected_jgst = None
+    # if request.GET.get('fach_filter') and request.GET.get('fach_filter') != '0' and request.GET.get('jgst_filter') and request.GET.get('jgst_filter') != '0':
+    #     selected_jgst = request.GET.get('jgst_filter')
+    #     mkr_objects = Kompetenzkarte.objects.filter(
+    #         fach=request.GET.get('fach_filter'), jgst=request.GET.get('jgst_filter')
+    #     )
+    # elif request.GET.get('fach_filter'):
+    #     mkr_objects = Kompetenzkarte.objects.filter(fach=request.GET.get('fach_filter'))
+    # elif request.GET.get('jgst_filter') and request.GET.get('jgst_filter') != '0':
+    #     selected_jgst = request.GET.get('jgst_filter')
+    #     mkr_objects = Kompetenzkarte.objects.filter(jgst=request.GET.get('jgst_filter'))
+    # else:
+    #     mkr_objects = Kompetenzkarte.objects.all()
+
+    if request.GET.get('switch') == "on":
+        switch = "on"
+    else:
+        switch = None
+
+    return render(request, 'mkr.html', {
+        'mkr_objects': mkr_objects,
+        'cookiebanner': cookiebanner,
+        'faecher': Fach.objects.all(),
+        'selected_fach': selected_fach,
+        'jgst_choices': Kompetenzkarte.JGST_CHOICES,
+        'selected_jgst': selected_jgst,
+        'switch': switch,
+        }
+    )
 
 
 @login_required
